c32aecc21a67d4b7d93011bf5627c6d9b61abf7f
Alexandre JANNOTTA <alexandre.jannotta@cgi.com>
2019-12-10 12:51:50 +0100

    Refactoring

--- white-core/src/main/java/com/cgi/paris/fs/comex/white/core/service/VatService.java
+++ white-core/src/main/java/com/cgi/paris/fs/comex/white/core/service/VatService.java
@@ -8,16 +8,17 @@ public class VatService {
     private static final BigDecimal FRENCH_VAT = new BigDecimal("0.2");
     private static final BigDecimal GERMAIN_VAT = new BigDecimal("0.19");
 
-    public double computeFrenchVat(final double i) {
-        return BigDecimal.valueOf(i)
-                .multiply(FRENCH_VAT)
-                .setScale(2, RoundingMode.HALF_UP)
-                .doubleValue();
+    public double computeFrenchVat(final double amount) {
+        return this.computeVat(amount, FRENCH_VAT);
     }
 
-    public double computeGermainVat(final double i) {
-        return BigDecimal.valueOf(i)
-                .multiply(GERMAIN_VAT)
+    public double computeGermainVat(final double amount) {
+        return this.computeVat(amount, GERMAIN_VAT);
+    }
+
+    private double computeVat(final double amount, final BigDecimal vat) {
+        return BigDecimal.valueOf(amount)
+                .multiply(vat)
                 .setScale(2, RoundingMode.HALF_UP)
                 .doubleValue();
     }
